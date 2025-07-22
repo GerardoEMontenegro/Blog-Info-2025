4a1e20acb11c2f948e8f896ac75c5e052e947f8f
cyruzzzz <montengr90@gmail.comgit config --global user.email montengr90@gmail.comgit config --global user.email montengr90@gmail.com>
2025-07-22 09:11:20 -0300

    Add footer, header, and navbar components with responsive design
    - Implemented a new footer component with social media links, quick links, and contact information.
    - Created a header component that includes the navbar.
    - Developed a navbar component with links to the homepage and user profile, along with a search bar and notification icon.
    - Ensured all components are styled using Tailwind CSS for a modern look and responsive behavior.

--- blog/apps/user/models.py
+++ blog/apps/user/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+import os
+
+def get_avatar_filename(instance, filename):
+    base_filename , file_extension = os.path.splitext(filename)
+    new_filename = f"user_{instance.id}_avatar{file_extension}"
+    return os.path.join('user/avastar/', new_filename)
+
+    pass
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    alias = models.CharField(max_length=30, unique=True, blank=True)
+    avatar = models.ImageField(upload_to=get_avatar_filename, default='user/default/default-avatar-2.png')
+
+    def __str__(self):
+        return self.username
+    
+    def get_avartar_url(self):
+        if self.avatar and hasattr(self.avatar, 'url'):
+            return self.avatar.url
+        return None
 
 # Create your models here.
